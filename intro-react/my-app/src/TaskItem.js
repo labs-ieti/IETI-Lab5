2395a502b6bf1eb1e6ccf9f83e55afad857e26d5
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-10-06 15:11:33 -0500

    Adicion de tareas

--- intro-react/my-app/src/TaskItem.js
+++ intro-react/my-app/src/TaskItem.js
@@ -1,10 +1,13 @@
 export const TaskItem = (props) =>{
 const {isChecked, taskName} = props;
+const styleOfheComponents ={
+    textDecoration: isChecked? "line-through":"",
+};
 
     return(
         <li>
          <input checked={isChecked} type="checkbox"/>
-         <span style={}>{taskName}</span>
+         <span style={styleOfheComponents}>{taskName}</span>
        </li>
     );
 };
